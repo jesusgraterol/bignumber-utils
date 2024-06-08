eeb7c9385d5b97a646dc5816621b14f82131ec5a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-06-08 15:17:49 -0400

    Complete the initial set of tests
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/utils/utils.test-unit.ts
+++ src/utils/utils.test-unit.ts
@@ -8,7 +8,23 @@ import { getBigNumber } from './utils.js';
  ************************************************************************************************ */
 
 describe('getBigNumber', () => {
-  test('can instantiate BigNumber with a number value', () => {
-    expect(getBigNumber(100.54)).toBeInstanceOf(BigNumber);
+  test.each([
+    { a: -854.11 },
+    { a: 100.54 },
+    { a: 154124564.655 },
+    { a: '-4561.551' },
+    { a: '100.54' },
+    { a: '9845418.455561' },
+    { a: BigNumber(-854.11) },
+    { a: BigNumber(100.54) },
+    { a: BigNumber('9845418.455561') },
+    { a: BigNumber('-4561.551') },
+  ])('can instantiate BigNumber with any number value', ({ a }) => {
+    const bn = getBigNumber(a);
+    expect(bn).toBeInstanceOf(BigNumber);
+    expect(BigNumber.isBigNumber(bn)).toBe(true);
+    expect(bn.toNumber()).toBe(Number(a));
   });
+
+
 });
